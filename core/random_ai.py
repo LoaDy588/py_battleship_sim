980dc215d34ea60b9456e4b22f789c42be784a56
LoaDy588@ <LoaDy588@seznam.cz>
2017-10-02 14:52:26 +0200

    working random ai

---
+++ core/random_ai.py
@@ -0,0 +1,16 @@
+import player
+import random
+
+
+class Random_AI(player.Player):
+    def __init__(self):
+        player.Player.__init__(self)
+        self.hit_list = []
+        for x in range(10):
+            for y in range(10):
+                self.hit_list.append([x, y])
+
+    def turn(self):
+        coords = self.hit_list[random.randint(0, len(self.hit_list)-1)]
+        self.hit_list.remove(coords)
+        return coords
