92c2cfa9b6f6b2425db40aaea36abeaebdfd65da
O. Pecák <oldrich.pecak@gmail.com>
2017-12-26 00:36:19 +0100

    working probabilistic AI

--- core/probabilistic_ai.py
+++ core/probabilistic_ai.py
@@ -1,3 +1,4 @@
+import random
 from . import player
 from . import field_utils
 
@@ -20,7 +21,7 @@ class Probabilistic_AI(player.Player):
         if ship_hit:
             self.__enemy_field[coords[0]][coords[1]]["content"] = "ship"
         if ship_sunk:
-            self.__enemy_field = sink_ship(self.__enemy_field, coords, length)
+            self.__sink_ship(self.__enemy_field, coords, length)
             for ship in self.__enemy_shiplist:
                 if ship["length"] == length:
                     self.__enemy_shiplist.remove(ship)
@@ -29,97 +30,102 @@ class Probabilistic_AI(player.Player):
     def __generate_target(self):
         probability = self.__create_probab_field()
         target = find_highest(probability)
-        print(target)
-        return target
+        if len(target) == 1:
+            return target[0]
+        else:
+            return target[random.randint(0, len(target)-1)]
 
     def __create_probab_field(self):
-        possible_points = generate_possible_points(self.__enemy_field)
+        possible_points = self.__generate_possible_points(self.__enemy_field)
         probab_field = [[0 for y in range(10)] for x in range(10)]
         for ship in self.__enemy_shiplist:
             for point in possible_points:
-                orientations, bonus = find_orientations(self.__enemy_field,
+                orientations, bonus = self.__find_orientations(
+                                                        self.__enemy_field,
                                                         point, ship["length"])
                 for index, orientation in enumerate(orientations):
-                    add_to_field(self.__enemy_field, probab_field, orientation,
-                                 bonus[index], point, ship["length"])
+                    self.__add_to_field(self.__enemy_field, probab_field,
+                                        orientation, bonus[index], point,
+                                        ship["length"])
         return probab_field
+
+    def __find_orientations(self, coords, length):
+        possible = []
+        possible_bonus = []
+        vectors = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        for vctr in vectors:
+            # if outside the field, skip to next orientation
+            if coords[0]+length*vctr[0] > 9 or coords[1]+length*vctr[1] > 9:
+                continue
+            if coords[0]+length*vctr[0] < 0 or coords[1]+length*vctr[1] < 0:
+                continue
+            temp = []
+            bonus = False
+            for i in range(length):
+                pointer = self.__enemy_field[coords[0]+i*vctr[0]][coords[1]+i*vctr[1]]
+                if not pointer["hit"]:
+                    temp.append(True)
+                elif pointer["hit"]:
+                    if pointer["content"] == "ship" and not pointer["sunk"]:
+                        temp.append(True)
+                        bonus = True
+            if sum(temp) == length:
+                possible.append(vctr)
+                possible_bonus.append(bonus)
+        return tuple(possible), tuple(possible_bonus)
+
+    def __add_to_field(self, probab_field, direction, bonus, point, length):
+        for i in range(length):
+            pointer = (point[0]+i*direction[0], point[1]+i*direction[1])
+            if not self.__enemy_field[pointer[0]][pointer[1]]["hit"]:
+                if bonus:
+                    probab_field[pointer[0]][pointer[1]] += 100
+                else:
+                    probab_field[pointer[0]][pointer[1]] += 1
+
+    def __generate_possible_points(self):
+        possible = []
+        for x in range(10):
+            for y in range(10):
+                pointer = self.__enemy_field[x][y]
+                if pointer[x][y]["content"] == "water" and not pointer[x][y]["hit"]:
+                    possible.append((x, y))
+                elif pointer[x][y]["content"] == "ship" and not pointer[x][y]["sunk"]:
+                    possible.append((x, y))
+        return possible
+
+    def __sink_ship(self, coords, length):
+        vectors = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        direction = ()
+        for vctr in vectors:
+            # if outside the field, skip to next orientation
+            if coords[0]+length*vctr[0] > 9 or coords[1]+length*vctr[1] > 9:
+                continue
+            if coords[0]+length*vctr[0] < 0 or coords[1]+length*vctr[1] < 0:
+                continue
+            temp = []
+            for i in range(length):
+                pointer = self.__enemy_field[coords[0]+i*vctr[0]][coords[1]+i*vctr[1]]
+                if pointer["content"] == "ship" and not pointer["sunk"]:
+                    temp.append(pointer["hit"])
+            if sum(temp) == length:
+                direction = vctr
+                break
+        if direction == ():
+            return
+        for i in range(length):
+            removal = (coords[0]+i*direction[0], coords[1]+i*direction[1])
+            self.__enemy_field[removal[0]][removal[1]]["sunk"] = True
 
 
 def find_highest(probab_field):
-    current = [0, 0]
+    current = [(0, 0)]
     current_value = 0
     for x in range(10):
         for y in range(10):
             if probab_field[x][y] > current_value:
-                current = [x, y]
+                current = [(x, y)]
                 current_value = probab_field[x][y]
-    return tuple(current)
-
-
-def find_orientations(field, coords, length):
-    possible = []
-    possible_bonus = []
-    vectors = ((1, 0), (0, 1), (-1, 0), (0, -1))
-    for vctr in vectors:
-        # if outside the field, skip to next orientation
-        if coords[0]+length*vctr[0] > 9 or coords[1]+length*vctr[1] > 9:
-            continue
-        if coords[0]+length*vctr[0] < 0 or coords[1]+length*vctr[1] < 0:
-            continue
-        temp = []
-        bonus = False
-        for i in range(length):
-            pointer = field[coords[0]+i*vctr[0]][coords[1]+i*vctr[1]]
-            if not pointer["hit"]:
-                temp.append(True)
-                if pointer["content"] == "ship":
-                    bonus = True
-        if sum(temp) == length:
-            possible.append(vctr)
-            possible_bonus.append(bonus)
-
-    return tuple(possible), tuple(possible_bonus)
-
-
-def add_to_field(field, probab_field, direction, bonus, point, length):
-    for i in range(length):
-        pointer = (point[0]+i*direction[0], point[1]+i*direction[1])
-        if not field[pointer[0]][pointer[1]]["hit"]:
-            if bonus:
-                probab_field[pointer[0]][pointer[1]] += 10
-            else:
-                probab_field[pointer[0]][pointer[1]] += 1
-
-
-def generate_possible_points(field):
-    possible = []
-    for x in range(10):
-        for y in range(10):
-            if field[x][y]["content"] == "water" and not field[x][y]["hit"]:
-                possible.append((x, y))
-            elif field[x][y]["content"] == "ship" and not field[x][y]["sunk"]:
-                possible.append((x, y))
-    return possible
-
-
-def sink_ship(field, coords, length):
-    vectors = ((1, 0), (0, 1), (-1, 0), (0, -1))
-    direction = ()
-    for vctr in vectors:
-        # if outside the field, skip to next orientation
-        if coords[0]+length*vctr[0] > 9 or coords[1]+length*vctr[1] > 9:
-            continue
-        if coords[0]+length*vctr[0] < 0 or coords[1]+length*vctr[1] < 0:
-            continue
-        temp = []
-        for i in range(length):
-            pointer = field[coords[0]+i*vctr[0]][coords[1]+i*vctr[1]]
-            if not pointer["sunk"]:
-                temp.append(pointer["hit"])
-        if sum(temp) == length:
-            direction = vctr
-            break
-
-    for i in range(length):
-        removal = (coords[0]+i*direction[0], coords[1]+i*direction[1])
-        field[removal[0]][removal[1]]["sunk"] = True
+            elif probab_field[x][y] == current_value:
+                current.append((x, y))
+    return current
